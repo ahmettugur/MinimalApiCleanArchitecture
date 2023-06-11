abd085634c3b936142812b0ac8a7f8ca41a795c2
ahmettugur <ahmet_tgr@hotmail.com>
2023-06-11 10:12:46 +0300

    Add migration

---
+++ src/Infrastructure/MinimalApiCleanArchitecture.Persistence/Migrations/20230611071219_Initial.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MinimalApiCleanArchitecture.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Authors",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Bio = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BlogId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Blogs",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AuthorId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blogs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Blogs_Authors_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Authors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Authors_BlogId",
+                table: "Authors",
+                column: "BlogId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Blogs_AuthorId",
+                table: "Blogs",
+                column: "AuthorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Authors_Blogs_BlogId",
+                table: "Authors",
+                column: "BlogId",
+                principalTable: "Blogs",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Authors_Blogs_BlogId",
+                table: "Authors");
+
+            migrationBuilder.DropTable(
+                name: "Blogs");
+
+            migrationBuilder.DropTable(
+                name: "Authors");
+        }
+    }
+}
---
+++ src/Infrastructure/MinimalApiCleanArchitecture.Persistence/ThoughtfulDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using MinimalApiCleanArchitecture.Domain.Model;
+
+namespace MinimalApiCleanArchitecture.Persistence;
+
+public class ThoughtfulDbContext : DbContext
+{
+    public ThoughtfulDbContext(DbContextOptions options) : base(options)
+    {
+
+    }
+    
+    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<Author> Authors { get; set; }
+}
+
+public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ThoughtfulDbContext>
+{
+    public ThoughtfulDbContext CreateDbContext(string[] args)
+    {
+        var connectionString = Environment.GetEnvironmentVariable("Default");
+        var builder = new DbContextOptionsBuilder<ThoughtfulDbContext>();
+        if (string.IsNullOrEmpty(connectionString))
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                //.AddJsonFile(@Directory.GetCurrentDirectory() + "/../Thoughtful.Api/appsettings.json")
+                .AddJsonFile("appsettings.json")
+                .Build();
+            connectionString = configuration.GetConnectionString("Default");
+        }
+        builder.UseSqlServer(connectionString);
+        return new ThoughtfulDbContext(builder.Options);
+    }
+}
