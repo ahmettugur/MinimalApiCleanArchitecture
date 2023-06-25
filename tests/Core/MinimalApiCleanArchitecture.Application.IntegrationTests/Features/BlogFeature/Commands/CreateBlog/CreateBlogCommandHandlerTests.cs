df24105c3195207d859287e63c4229246d38742d
ahmettugur <ahmet_tgr@hotmail.com>
2023-06-25 09:30:31 +0300

    Coding Application Integration Tests

--- tests/Core/MinimalApiCleanArchitecture.Application.IntegrationTests/Features/BlogFeature/Commands/CreateBlog/CreateBlogCommandHandlerTests.cs
+++ tests/Core/MinimalApiCleanArchitecture.Application.IntegrationTests/Features/BlogFeature/Commands/CreateBlog/CreateBlogCommandHandlerTests.cs
@@ -1,5 +1,7 @@
 using FluentAssertions;
 using MinimalApiCleanArchitecture.Application.Common.Exceptions;
+using MinimalApiCleanArchitecture.Application.Common.Results;
+using MinimalApiCleanArchitecture.Application.Features.AuthorFeature.Commands.CreateAuthor;
 using MinimalApiCleanArchitecture.Application.Features.BlogFeature.Commands.CreateBlog;
 
 namespace MinimalApiCleanArchitecture.Application.IntegrationTests.Features.BlogFeature.Commands.CreateBlog;
@@ -15,5 +17,33 @@ public class CreateBlogCommandHandlerTests: BaseTestFixture
 
         await FluentActions.Invoking(() =>
             SendAsync(command)).Should().ThrowAsync<ValidationException>();
+    }
+
+    [Fact]
+    public async Task TestCreateBlog_CreateBlogWithInvalidAuthor_ShouldReturn_ErrorDataResult()
+    {
+        var command = new CreateBlogCommand("C¢QRS & Event Sourcing","Microservices",Guid.NewGuid());
+
+        var result = await SendAsync(command);
+        result.Should().BeAssignableTo<ErrorDataResult<CreateBlogResponse>>();
+        result.Message.Should().Be($"Author cannot found with id: {command.AuthorId}");
+        result.Data.Should().BeNull();
+    }
+
+    
+    [Fact]
+    public async Task TestCreateBlog_CreateBlogWithValidCommandShouldReturn_SuccessDataResult()
+    {
+        var authorCommand = new CreateAuthorCommand("Jon", "Doe", "Developer", new DateTime(1990, 9, 1));
+        var authorResult = await SendAsync(authorCommand);
+
+        authorResult.Should().BeAssignableTo<CreateAuthorResponse>();
+        
+        var blogCommand = new CreateBlogCommand("C¢QRS & Event Sourcing","Microservices",authorResult.Id);
+        var blogResult = await SendAsync(blogCommand);
+        
+        blogResult.Data.Owner.Should().NotBeNull();
+        blogResult.Message.Should().Be("Success");
+        blogResult.Data.Should().BeOfType<CreateBlogResponse>();
     }
 }
